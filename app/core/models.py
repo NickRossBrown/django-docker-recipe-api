3dc1232c3b49904bc0e9e0f5eedff5d0e9ab13e8
Nick Brown <nickrossbrown@gmail.com>
2019-04-30 02:48:08 -0700

    Completed section 8

--- app/core/models.py
+++ app/core/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
+    PermissionsMixin
 
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """ Creates and saves a new user """
-        user = self.model(email=email.self.normalize_email, **extra_fields)
+        """Creates and saves a new User"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_super_user(self, email, password):
-        """ Creates a super user """
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -24,9 +27,9 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """ Creates user model that supports email instead of username """
-    email = models.EmailField(max_length=225, unique=True)
-    name = models.CharField(max_length=225)
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
