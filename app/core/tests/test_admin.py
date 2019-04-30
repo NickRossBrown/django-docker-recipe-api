9ca1b3edc975a452cf17006bbdbcac8ca40403d9
Nick Brown <nickrossbrown@gmail.com>
2019-04-30 02:51:00 -0700

    Section 9 tests pass

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -19,17 +19,24 @@ class AdminSiteTests(TestCase):
             name='Test User Full Name',
         )
 
-    # def test_users_listed(self):
-    #     """Test that users are listed on the user page"""
-    #     url = reverse('admin:core_user_changelist')
-    #     res = self.client.get(url)
+    def test_users_listed(self):
+        """Test that users are listed on the user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
 
-    #     self.assertContains(res, self.user.name)
-    #     self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
 
-#     def test_user_page_change(self):
-#         """Test that the user edit page works"""
-#         url = reverse('admin:core_user_change', args=[self.user.id])
-#         res = self.client.get(url)
+    def test_user_page_change(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
 
-#         self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
