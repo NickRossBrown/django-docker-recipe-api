dab9838493c246354a4583520f9d0fe4d3bb8b35
Nick Brown <nickrossbrown@gmail.com>
2019-04-30 02:32:42 -0700

    add admin super

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ docker-compose run app sh -c "django-admin startproject app ."
 docker-compose run app sh -c "python manage.py startapp core"
 
 docker-compose run app sh -c "python manage.py makemigrations core"
+
+docker-compose run app sh -c "python manage.py test"
+
+docker-compose run app sh -c "python manage.py test && flake8"
--- app/core/models.py
+++ app/core/models.py
@@ -1,18 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ Creates and saves a new user """
-        user = self.model(email, **extra_fields)
+        user = self.model(email=email.self.normalize_email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_super_user(self, email, password):
+        """ Creates a super user """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
+    """ Creates user model that supports email instead of username """
     email = models.EmailField(max_length=225, unique=True)
     name = models.CharField(max_length=225)
     is_active = models.BooleanField(default=True)
